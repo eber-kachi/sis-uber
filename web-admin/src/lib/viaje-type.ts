d8adec962f69bd193097790d6558ba7640c632c3
eber-kachi <eber.nina96@gmail.com>
2023-05-30 10:08:03 -0400

    tipos evento viaje

---
+++ web-admin/src/lib/viaje-type.ts
@@ -0,0 +1,17 @@
+"use strict";
+
+export enum EstadoViaje {
+  PENDIENTECONFIRMACION = "pendiente_confirmacion",
+  CONFIRMADO = "confirmado",
+  ENPROGRESO = "en_progreso",
+  FINALIZADO = "finalizado",
+  CanceladoUsuario = "cancelado_usuario",
+  CanceladoConductor = "cancelado_conductor",
+  CanceladoSistema = "cancelado_sistema",
+}
+
+export enum StateHandlerEvents {
+  VIAJEPENDIENTECONFIRMACIONEVENTO = "viaje_pendiente_confirmacion_evento",
+  SOCIOACTIVOEVENTO = "socio_activo_evento",
+  SOCIOINACTIVOEVENTO = "socio_inactivo_evento",
+}
